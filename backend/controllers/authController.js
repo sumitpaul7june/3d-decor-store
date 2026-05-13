1e52d5f0a6cdccadfd76d0461b8517704f4e2dcf
Sumit <73896616+sumitpaul7june@users.noreply.github.com>
2026-05-13 14:48:47 +0530

    configuration problem fixed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -11,8 +11,8 @@ const passwordRegex =
 
 const cookieOptions = {
   httpOnly: true,
-  secure: false, // true in production with HTTPS
-  sameSite: "strict",
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
   maxAge: 7 * 24 * 60 * 60 * 1000
 };
 
